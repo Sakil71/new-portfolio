24a60ade3e531c5aaaf77476db4fb75e242a57bf
Shakil Ahamed <sakilahmed7810@gmail.com>
2023-01-20 00:23:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import arrow from './assets/arrow-left-solid.svg'
+import { FaHome } from 'react-icons/fa';
 
 function App() {
+  const [open, setOpen] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex'>
+      <div className={`${open ? 'w-72' : 'w-20'} duration-500 w-72 h-screen bg-dark-purple relative p-5`}>
+        <img
+          className={`absolute cursor-pointer -right-3 top-8 w-7 border-2 p-1 border-dark-purple rounded-full bg-white ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+          src={arrow} alt="" />
+
+        <div className='flex gap-x-4 items-center'>
+          <FaHome className='text-white cursor-pointer duration-500 text-3xl'></FaHome>
+          <h1 className='text-white origin-left font-medium text-xl duration-300'>Home</h1>
+        </div>
+      </div>
+
+      <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
+        <h1>Home Page</h1>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255, 2555, 255, 0.17)"
+      }
+    },
+  },
+
+  plugins: [require("daisyui")],
+}
